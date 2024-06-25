29bc2a0a1b2d712c89936702108c762213f9e847
Vityuk Yuriy <you.thunderstruck@gmail.com>
2024-06-26 01:08:43 +0300

    task 1,2,3,4 resolving

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,60 @@
+class Key {
+    private signature:number
+    constructor(){
+        this.signature = Math.random()
+    }
+
+    getSignature ():number {
+        return this.signature
+    }
+}
+class Person {
+    constructor(private key:Key) {};
+    getKey ():Key {
+        return this.key
+    }
+}
+abstract class House {
+    public door:boolean;
+    protected key:Key;
+    public tenants: Person[]
+    constructor( key:Key) {
+        this.tenants = []
+        this.key = key;
+    }
+    public comeIn (person:Person):void {
+        if (this.door) {
+            this.tenants.push(person)
+        }
+    }
+    public showTenants ():void {
+        console.log(this.tenants)
+    }
+    public abstract openDoor (key:Key):void;
+}
+class MyHouse extends House {
+   
+    constructor(key:Key) {
+        super (key); 
+    }
+
+    public openDoor(key: Key): void {
+        if(this.key == key){
+            this.door = true;
+        } else {console.log ("Bad key")}
+        
+
+    }
+}
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
+house.showTenants()
 
-
-export {};
